73f8c4fb970f8f3eb92fabe53489163f8870857e
Michael Kovacsik <kovacsikdev@gmail.com>
2021-07-11 16:45:16 -0500

    refactor and simplify the ThemeSlider

--- src/components/ThemeSlider/ThemeSlider.jsx
+++ src/components/ThemeSlider/ThemeSlider.jsx
@@ -1,39 +1,18 @@
 import React from 'react';
-import { themes } from './themes';
+import { theme } from '../../libs/theme/theme'
 import sunIcon from '../../assets/sun-icon.png';
 import nightIcon from '../../assets/night-icon.png';
 import './ThemeSlider.scss';
 
 export const ThemeSlider = () => {
-  const [themeName, setThemeName] = React.useState(themes.themeName);
-
-  const setCSSVariables = theme => {
-    for (const value in theme) {
-      document.documentElement.style.setProperty(`--${value}`, theme[value]);
-    }
-  };
+  const [themeName, setThemeName] = React.useState(theme.getTheme());
 
   const toggleTheme = () => {
-    if (themeName === 'dark') {
-      setThemeName('light');
-      localStorage.setItem('themeName', 'light');
-    } else {
-      setThemeName('dark');
-      localStorage.setItem('themeName', 'dark');
-    }
+    const newTheme = themeName === 'light' ? 'dark' : 'light';
+    setThemeName(newTheme);
+    localStorage.setItem('themeName', newTheme);
+    theme.setTheme(newTheme);
   };
-
-  React.useEffect(() => {
-    const savedTheme = localStorage.getItem('themeName');
-
-    if(savedTheme){
-      setThemeName(savedTheme);
-    }
-  }, []);
-
-  React.useEffect(() => {
-    setCSSVariables(themes[themeName]);
-  }, [themeName]);
 
   return (
     <div className="ThemeSlider">
